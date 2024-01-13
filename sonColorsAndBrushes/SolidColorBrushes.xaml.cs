0315db3ccc154606b15cbc3fee96d722b7d04ca2
JoonKai <joonseok831@gmail.com>
2024-01-14 05:55:36 +0900

    초기 커밋.

---
+++ sonColorsAndBrushes/SolidColorBrushes.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Samples.CustomControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace sonColorsAndBrushes
+{
+    /// <summary>
+    /// SolidColorBrushes.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class SolidColorBrushes : Window
+    {
+        public SolidColorBrushes()
+        {
+            InitializeComponent();
+
+            SolidColorBrush brush = new SolidColorBrush();
+
+            brush = Brushes.Red;
+            rect1.Fill = brush;
+
+            brush = new SolidColorBrush(Colors.Green);
+            rect2.Fill = brush;
+
+            brush = new SolidColorBrush(Color.FromArgb(100, 0, 0, 255));
+            rect3.Fill = brush;
+
+            brush = new SolidColorBrush(Color.FromScRgb(0.5f, 0.7f, 0.0f, 0.5f));
+            rect4.Fill = brush;
+
+            brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#CBFFFFAA"));
+            rect5.Fill = brush;
+
+        }
+
+        private void ChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            ColorPickerDialog cPicker = new ColorPickerDialog();
+            cPicker.StartingColor = Colors.LightBlue;
+            cPicker.Owner = this;
+            bool? dialogResult = cPicker.ShowDialog();
+            if(dialogResult !=null &&(bool)dialogResult == true)
+            {
+                rect6.Fill = new SolidColorBrush(cPicker.SelectedColor);
+            }
+        }
+    }
+}
---
+++ sonColorsAndBrushes/StartMenu.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+
+namespace sonColorsAndBrushes
+{
+    /// <summary>
+    /// StartMenu.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class StartMenu : Window
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Color_Click(object sender, RoutedEventArgs e)
+        {
+            SystemColors sc = new SystemColors();
+            sc.ShowDialog();
+        }
+        private void ColorPicker_Click(object sender, RoutedEventArgs e)
+        {
+            ColorPicker cp = new ColorPicker();
+            cp.ShowDialog();
+        }
+
+        private void SolidColorBrushes_Click(object sender, RoutedEventArgs e)
+        {
+            SolidColorBrushes scb = new SolidColorBrushes();
+            scb.ShowDialog();
+        }
+
+        private void LinearGradientBrushes_Click(object sender, RoutedEventArgs e)
+        {
+            LinearGradientBrushes lGb = new LinearGradientBrushes();
+            lGb.ShowDialog();
+        }
+
+        private void RadialGradientBrush_Click(object sender, RoutedEventArgs e)
+        {
+            RadialGradientBrush rgb = new RadialGradientBrush();
+            rgb.ShowDialog();
+        }
+
+        private void DrawingBrushes_Click(object sender, RoutedEventArgs e)
+        {
+            DrawingBrushes db = new DrawingBrushes();
+            db.ShowDialog();
+        }
+
+        private void CustomColormapBrushes_Click(object sender, RoutedEventArgs e)
+        {
+            CustomColormapBrushes ccb = new CustomColormapBrushes();
+            ccb.ShowDialog();
+        }
+
+        private void CustomColorShading_Click(object sender, RoutedEventArgs e)
+        {
+            CustomColorShading ccs = new CustomColorShading();
+            ccs.ShowDialog();
+        }
+    }
+}
